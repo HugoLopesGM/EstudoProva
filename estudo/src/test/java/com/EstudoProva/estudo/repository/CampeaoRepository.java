4323c0079e1a36ef43f470fd73028f1010272e20
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-06-13 19:23:28 -0300

    repository

---
+++ estudo/src/test/java/com/EstudoProva/estudo/repository/CampeaoRepository.java
@@ -0,0 +1,11 @@
+package com.EstudoProva.estudo.repository;
+
+import com.EstudoProva.estudo.model.CampeaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CampeaoRepository extends JpaRepository<CampeaoModel, UUID> {
+}
